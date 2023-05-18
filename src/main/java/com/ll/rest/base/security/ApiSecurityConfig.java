a678fa86b2c5df4ab919781932c3b606af0e2d4d
Genie16-github <4411dlwoqja@naver.com>
2023-05-18 13:21:04 +0900

    JwtAuthorizationFilter

--- src/main/java/com/ll/rest/base/security/ApiSecurityConfig.java
+++ src/main/java/com/ll/rest/base/security/ApiSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ll.rest.base.security;
 
+import com.ll.rest.base.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -29,7 +34,11 @@ public class ApiSecurityConfig {
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
-                ); // 세션끄기
+                ) // 세션끄기
+                .addFilterBefore(
+                        jwtAuthorizationFilter, // 엑세스 토큰으로 부터 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
